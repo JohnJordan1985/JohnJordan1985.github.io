aefc4abda37c22955b22d1277411625102e1c080
johnjordan1985 <johnjordan1985@gmail.com>
2019-05-20 20:47:11 -0700

    Removes HTML elements from project descriptions, as too much effort to manually escape.. dangerous too.

--- js/app.js
+++ js/app.js
@@ -3,17 +3,17 @@ const ghpagesProjects = [
     name: "Caeser Cipher",
     projectURL: "https://johnjordan1985.github.io/caesar-cipher/",
     description: `A fully functional attempt to explain Public Key Encryption, which is the basis of most of the secure communication over the internet.`,
-    details: `Includes some of the history behind encryption, beginning with the illustrious Julius Caeser, and allows to user to <em>encrypt</em> and <em>decrypt</em> short text-based messages.`
+    details: `Includes some of the history behind encryption, beginning with the illustrious Julius Caeser, and allows to user to encrypt and decrypt short text-based messages.`
   },  {
     name: "FizzBuzz",
     projectURL: null,
     description: `After suggesting the 'nth-child' CSS selector to solve a front-end issue for a DevOps colleague, they mused that you could use CSS to solve the 'FizzBuzz' challenge. `,
-    details: `I didn't know what 'FizzBuzz' was, but after having it explained to me, I challenged myself to solve it using CSS3 <em>alone</em>: I simultaneously solved it using JavaScript, for comparison. The retro 90's eyeball-bleeding appearance is a bit of an in-joke...`
+    details: `I didn't know what 'FizzBuzz' was, but after having it explained to me, I challenged myself to solve it using CSS3 alone: I simultaneously solved it using JavaScript, for comparison. The retro 90's eyeball-bleeding appearance is a bit of an in-joke...`
   },  {
     name: "Revamped Confetti",
     projectURL: null,
     description: `An update of a university project, where I had to design a landing page for a "full sprectrum" wedding service called "Ready Confetti" `,
-    details: `Returned to this project to apply my learnings in typography, color theory and content writing, as well as to test out the newly stable Bootstrap 4 framework (the original project had used Bootstrap 3.`
+    details: `Returned to this project to apply my learnings in typography, color theory and content writing, as well as to test out the newly stable Bootstrap 4 framework (the original project had used Bootstrap 3).`
   },{
     name: "List-O-Rama",
     projectURL: null,
@@ -23,15 +23,12 @@ const ghpagesProjects = [
     name: "Galway Pub Finder",
     projectURL: null,
     description: `A project to build a web application using knockout.js, which integrated with Google Maps and the Yelp API, to display various pubs on a map.`,
-    details: `My first web application. It was fun to build, but not so fun to use: it doesn't display well on mobile, and only displays a fraction of the pubs in my hometown. Not that pubs are <em>hard</em> to find in my hometown or anything...`
+    details: `My first web application. It was fun to build, but not so fun to use: it doesn't display well on mobile, and only displays a fraction of the pubs in my hometown. Not that pubs are hard to find in my hometown or anything...`
   }
 ];
 
 class Project extends React.Component {
 
-	// constructor(argument) {
-	// 	// code...
-	// }
 	render() {
 		const project = this.props.project;
 		return (
