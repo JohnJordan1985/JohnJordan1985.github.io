8755386a2fd96030d739aa7f851cbc1a1a63ff10
johnjordan1985 <johnjordan1985@gmail.com>
2019-05-20 20:41:48 -0700

    Replaces dummy data with actual content dynamically constructed. Need to fix issue with HTML elements in project text elements.

--- js/app.js
+++ js/app.js
@@ -1,30 +1,30 @@
 const ghpagesProjects = [
-	{
-		name: "Caeser Cipher",
-		projectURL: "https://johnjordan1985.github.io/caesar-cipher/",
-		description: `<p>A fully functional attempt to explain Public Key Encryption, which is the basis of most of the secure communication over the internet.</p>`,
-		details: `<p>Includes some of the history behind encryption, beginning with the illustrious Julius Caeser, and allows to user to <em>encrypt</em> and <em>decrypt</em> short text-based messages.</p>`
-	},	{
-		name: "FizzBuzz",
-		projectURL: null,
-		description: `<p>After suggesting the 'nth-child' CSS selector to solve a front-end issue for a DevOps colleague, they mused that you could use CSS to solve the 'FizzBuzz' challenge. </p>`,
-		details: `<p>I didn't know what 'FizzBuzz' was, but after having it explained to me, I challenged myself to solve it using CSS3 <em>alone</em>: I simultaneously solved it using JavaScript, for comparison.</p><p>The retro 90's eyeball-bleeding appearance is a bit of an in-joke...</p>`
-	},	{
-		name: "Revamped Confetti",
-		projectURL: null,
-		description: `<p>An update of a university project, where I had to design a landing page for a "full sprectrum" wedding service called "Ready Confetti" </p>`,
-		details: `<p>Returned to this project to apply my learnings in typography, color theory and content writing, as well as to test out the newly stable Bootstrap 4 framework (the original project had used Bootstrap 3.</p>`
-	},{
-		name: "List-O-Rama",
-		projectURL: null,
-		description: `<p>A university project to build a simple jQuery "To-Do List" application.</p>`,
-		details: `<p>A straight-forward "To-Do List" application, which uses localStorage to store user input and allow use offline.</p>`
-	},	{
-		name: "Galway Pub Finder",
-		projectURL: null,
-		description: `<p>A project to build a web application using knockout.js, which integrated with Google Maps and the Yelp API, to display various pubs on a map.</p>`,
-		details: `<p>My first web application. It was fun to build, but not so fun to use: it doesn't display well on mobile, and only displays a fraction of the pubs in my hometown. Not that pubs are <em>hard</em> to find in my hometown or anything...</p>`
-	}
+  {
+    name: "Caeser Cipher",
+    projectURL: "https://johnjordan1985.github.io/caesar-cipher/",
+    description: `A fully functional attempt to explain Public Key Encryption, which is the basis of most of the secure communication over the internet.`,
+    details: `Includes some of the history behind encryption, beginning with the illustrious Julius Caeser, and allows to user to <em>encrypt</em> and <em>decrypt</em> short text-based messages.`
+  },  {
+    name: "FizzBuzz",
+    projectURL: null,
+    description: `After suggesting the 'nth-child' CSS selector to solve a front-end issue for a DevOps colleague, they mused that you could use CSS to solve the 'FizzBuzz' challenge. `,
+    details: `I didn't know what 'FizzBuzz' was, but after having it explained to me, I challenged myself to solve it using CSS3 <em>alone</em>: I simultaneously solved it using JavaScript, for comparison. The retro 90's eyeball-bleeding appearance is a bit of an in-joke...`
+  },  {
+    name: "Revamped Confetti",
+    projectURL: null,
+    description: `An update of a university project, where I had to design a landing page for a "full sprectrum" wedding service called "Ready Confetti" `,
+    details: `Returned to this project to apply my learnings in typography, color theory and content writing, as well as to test out the newly stable Bootstrap 4 framework (the original project had used Bootstrap 3.`
+  },{
+    name: "List-O-Rama",
+    projectURL: null,
+    description: `A university project to build a simple jQuery "To-Do List" application.`,
+    details: `A straight-forward "To-Do List" application, which uses localStorage to store user input and allow use offline.`
+  },  {
+    name: "Galway Pub Finder",
+    projectURL: null,
+    description: `A project to build a web application using knockout.js, which integrated with Google Maps and the Yelp API, to display various pubs on a map.`,
+    details: `My first web application. It was fun to build, but not so fun to use: it doesn't display well on mobile, and only displays a fraction of the pubs in my hometown. Not that pubs are <em>hard</em> to find in my hometown or anything...`
+  }
 ];
 
 class Project extends React.Component {
@@ -33,24 +33,23 @@ class Project extends React.Component {
 	// 	// code...
 	// }
 	render() {
-
+		const project = this.props.project;
 		return (
 			<li>
 				<h2>
-					<a href="#">Heading</a>
+					<a href={project.projectURL}>{project.name}</a>
 				</h2>
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic ad laborum qui ipsam.
+					{project.description}
 				</p>
 				<details>
 					<summary>
-						Repudiandae libero illo ex, rem commodi.
+						Click to read more...
 					</summary>
 					<p>
-						Necessitatibus iusto ut autem ducimus quo porro ullam dignissimos earum officiis.
+						{project.details}
 					</p>
 				</details>
-
 			</li>
 		);
 	}
@@ -61,16 +60,13 @@ class ProjectList extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			projects: this.props.projectList
+			projects: this.props.projectList.map((project, index) => <Project project={project} key={index} />)
 		};
 	}
 
 	render() {
-
-		const projects = this.state.projects.map((project, index) => <Project key={index} /> );
-
 		return (
-			<ul className="project-list">{projects}</ul>
+			<ul className="project-list">{this.state.projects}</ul>
 		);
 	}
 }
