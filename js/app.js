513ec9e0d67b85fa53e1f60eae1c9946393b56a0
johnjordan1985 <johnjordan1985@gmail.com>
2019-05-22 16:02:08 -0700

    Dynamically updates directory from GitHub APi call, and filters list within ProjectList Component

--- js/app.js
+++ js/app.js
@@ -18,52 +18,66 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 
 function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
 
-var allRepos = fetch("https://api.github.com/users/JohnJordan1985/repos").then(function (results) {
-  return results.json();
-}).then(function (data) {
-  console.log(data);
-});
 var listedRepos = [{
   name: "Caeser Cipher",
   id: 183995858,
+  description: "A fully functional attempt to explain Public Key Encryption, which is the basis of most of the secure communication over the internet.",
   details: "Includes some of the history behind encryption, beginning with the illustrious Julius Caeser, and allows the user to encrypt and decrypt short text-based messages."
 }, {
   name: "FizzBuzz",
   id: 182310084,
-  details: "I didn't know what 'FizzBuzz' was, but after having it explained to me, I challenged myself to solve it using CSS3 alone: I simultaneously solved it using JavaScript, for comparison. The retro 90's eyeball-bleeding appearance is a bit of an in-joke..."
-}, {
-  name: "Revamped Confetti",
-  id: 133503450,
-  details: "Returned to this project to apply my learnings in typography, color theory and content writing, as well as to test out the newly stable Bootstrap 4 framework (the original project had used Bootstrap 3)."
-}, {
-  name: "List-O-Rama",
-  id: 96696380,
-  details: "A straight-forward \"To-Do List\" application, which uses localStorage to store user input and allow use offline."
-}, {
-  name: "Galway Pub Finder",
-  id: 66479971,
-  details: "My first web application. It was fun to build, but not so fun to use: it doesn't display well on mobile, and only displays a fraction of the pubs in my hometown. Not that pubs are hard to find in my hometown or anything..."
-}];
-var ghpagesProjects = [{
-  name: "Caeser Cipher",
-  projectURL: "https://johnjordan1985.github.io/caesar-cipher/",
-  description: "A fully functional attempt to explain Public Key Encryption, which is the basis of most of the secure communication over the internet."
-}, {
-  name: "FizzBuzz",
-  projectURL: null,
+  details: "I didn't know what 'FizzBuzz' was, but after having it explained to me, I challenged myself to solve it using CSS3 alone: I simultaneously solved it using JavaScript, for comparison. The retro 90's eyeball-bleeding appearance is a bit of an in-joke...",
   description: "After suggesting the 'nth-child' CSS selector to solve a front-end issue for a DevOps colleague, they mused that you could use CSS to solve the 'FizzBuzz' challenge. "
 }, {
   name: "Revamped Confetti",
-  projectURL: null,
+  id: 133503450,
+  details: "Returned to this project to apply my learnings in typography, color theory and content writing, as well as to test out the newly stable Bootstrap 4 framework (the original project had used Bootstrap 3).",
   description: "An update of a university project, where I had to design a landing page for a \"full sprectrum\" wedding service called \"Ready Confetti\" "
 }, {
   name: "List-O-Rama",
-  projectURL: null,
+  id: 96696380,
+  details: "A straight-forward \"To-Do List\" application, which uses localStorage to store user input and allow use offline.",
   description: "A university project to build a simple jQuery \"To-Do List\" application."
 }, {
   name: "Galway Pub Finder",
-  projectURL: null,
+  id: 66479971,
+  details: "My first web application. It was fun to build, but not so fun to use: it doesn't display well on mobile, and only displays a fraction of the pubs in my hometown. Not that pubs are hard to find in my hometown or anything...",
   description: "A project to build a web application using knockout.js, which integrated with Google Maps and the Yelp API, to display various pubs on a map."
+}];
+
+var isListedRepo = function isListedRepo(repoId, repoList) {
+  var l = repoList.length;
+
+  for (var i = 0; i < l; i++) {
+    if (repoList[i].id === repoId) {
+      return true;
+    }
+  }
+
+  return false;
+}; // Not a repo ID
+
+
+console.log(isListedRepo(10, listedRepos)); // Repo ID, but not a listed repo
+
+console.log(isListedRepo(68470429, listedRepos)); // Listed repo ID
+
+console.log(isListedRepo(66479971, listedRepos));
+var ghpagesProjects = [{
+  name: "Caeser Cipher",
+  projectURL: "https://johnjordan1985.github.io/caesar-cipher/"
+}, {
+  name: "FizzBuzz",
+  projectURL: null
+}, {
+  name: "Revamped Confetti",
+  projectURL: null
+}, {
+  name: "List-O-Rama",
+  projectURL: null
+}, {
+  name: "Galway Pub Finder",
+  projectURL: null
 }];
 
 var Project =
@@ -82,7 +96,7 @@ function (_React$Component) {
     value: function render() {
       var project = this.props.project;
       return React.createElement("li", null, React.createElement("h2", null, React.createElement("a", {
-        href: project.projectURL
+        href: project.html_url
       }, project.name)), React.createElement("p", null, project.description), React.createElement("details", null, React.createElement("summary", null, "Click to read more..."), React.createElement("p", null, project.details)));
     }
   }]);
@@ -102,13 +116,7 @@ function (_React$Component2) {
 
     _this = _possibleConstructorReturn(this, _getPrototypeOf(ProjectList).call(this, props));
     _this.state = {
-      projects: _this.props.projectList.map(function (project, index) {
-        return React.createElement(Project, {
-          project: project,
-          key: index
-        });
-      }),
-      apiData: []
+      projects: []
     };
     return _this;
   }
@@ -116,10 +124,26 @@ function (_React$Component2) {
   _createClass(ProjectList, [{
     key: "componentDidMount",
     value: function componentDidMount() {
-      fetch("https://api.github.com/users/JohnJordan1985/repos").then(function (results) {
+      var _this2 = this;
+
+      var listedRepoList;
+      var allRepos = fetch("https://api.github.com/users/JohnJordan1985/repos?&per_page=100").then(function (results) {
         return results.json();
       }).then(function (data) {
-        console.log(data);
+        listedRepoList = data.filter(function (repo) {
+          return isListedRepo(repo.id, listedRepos);
+        });
+        console.log(listedRepoList);
+        var projects = listedRepoList.map(function (project, index) {
+          return React.createElement(Project, {
+            project: project,
+            key: index
+          });
+        });
+
+        _this2.setState({
+          projects: projects
+        });
       });
     }
   }, {
@@ -134,6 +158,4 @@ function (_React$Component2) {
   return ProjectList;
 }(React.Component);
 
-ReactDOM.render(React.createElement(ProjectList, {
-  projectList: ghpagesProjects
-}), document.getElementById("projectList_wrapper"));
+ReactDOM.render(React.createElement(ProjectList, null), document.getElementById("projectList_wrapper"));
